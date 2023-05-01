36f8b484621cac560ecb94b6564df529a2f1d821
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-05-01 17:52:13 +0200

    ejercicios terminados excepto el premium

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
-console.log(document.title);
+const titleGeneration1 = document.querySelector('#gen-1');
+const infoCardList = document.querySelector(".infocard-list");
+const images = document.querySelectorAll("img");
+const voladores = document.querySelectorAll(".infocard-lg-data text-muted, .itype flying")
+const infocard = document.querySelectorAll(".infocard")
+
+//1
+titleGeneration1.innerHTML = 'Generasión 1 Pokimon';
+
+//2
+infoCardList.style.backgroundColor = 'lightblue';
+
+//3
+console.log(window.location.href)
+
+//4
+console.log(images);
+
+//5
+/* for (let i = 0; i < images.length; i++) {
+    images[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+} */
+
+//6
+console.log(voladores)
+
+//7
+console.log(infocard)
