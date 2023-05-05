7b495fe1f2b1a2e95bed32d811f40d61303fdcd5
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-05-05 09:03:14 +0200

    correccion de errores

--- scripts.js
+++ scripts.js
@@ -4,25 +4,25 @@ const images = document.querySelectorAll("img");
 const voladores = document.querySelectorAll(".infocard-lg-data text-muted, .itype flying")
 const infocard = document.querySelectorAll(".infocard")
 
-//1
+//1 Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon".
 titleGeneration1.innerHTML = 'Generasión 1 Pokimon';
 
-//2
+//2 Cambia el color de fondo de la primera generación de Pokimon.
 infoCardList.style.backgroundColor = 'lightblue';
 
-//3
+//3 Imprime por consola la URL de la página.    
 console.log(window.location.href)
 
-//4
+//4 Imprime por consola el dominio de la página.
+
+//5 Imprime todos los nodos de imagen.
 console.log(images);
 
-//5
-/* for (let i = 0; i < images.length; i++) {
+
+//6 Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+for (let i = 0; i < images.length; i++) {
     images[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
-} */
-
-//6
-console.log(voladores)
-
-//7
+}
+//7 Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itype flying
 console.log(infocard)
+console.log(voladores)
